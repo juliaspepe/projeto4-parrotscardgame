0497baeedfbbaedf9bcfe08adee32b564807bab8
juliaspepe <devjuliapepe@gmail.com>
2022-08-19 10:25:11 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,9 @@
+prompt("Com quantas cartas quer jogar?");
+
+function QdCartas (){
+// se q qd de cartas for menos que 4 && maior que 14 && for um número impar 
+// perguntar novamente com o prompt para o jogador escolher cartas entre 4 e 14 e que sejam pares.
+// se o número de carta for equivalente a premissa, começar o jogo com o número de cartas selecionado
+
+}
+
